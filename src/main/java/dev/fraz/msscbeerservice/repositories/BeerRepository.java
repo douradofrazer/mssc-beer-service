da20ef40137bd8b4f60517f3f423c81ec8fdcd4a
Frazer Joe Dourado <douradofrazer@gmail.com>
2020-12-18 20:55:11 +0530

    Added JPA and bootstrap data

---
+++ src/main/java/dev/fraz/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,8 @@
+package dev.fraz.msscbeerservice.repositories;
+
+import dev.fraz.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {}
